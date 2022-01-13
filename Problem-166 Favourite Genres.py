ff99a127bfb7e481b159f3d4d4dc8a163a68e7df
Adhish Shrivastava <adhish70@gmail.com>
2022-01-13 10:25:50 -0500

    Problem 166 done

--- Problem-166 Favourite Genres.py
+++ Problem-166 Favourite Genres.py
@@ -1,11 +1,42 @@
 # Favourite Genres
 # https://github.com/adhish70/Hashing-3
 
-# Logic: 
-
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(no. of songs)
+# Space Complexity: O(no. of songs)
 
 class Solution:
     def favGenre(self, userSongs, genreSongs):
-        pass
+        result = dict()
+
+        # Make a song to genre hashmap
+        songGenre = dict()
+        for genre in genreSongs:
+            songList = genreSongs[genre]
+
+            for song in songList:
+                songGenre[song] = genre
+        
+        # For each user, conver the song list into a genre count hashmap
+        for user in userSongs.keys():
+            songList = userSongs[user]
+            genreCount = dict()
+            maxFreq = 0
+
+            # Count hashmap
+            for song in songList:
+                if songGenre[song] in genreCount:
+                    genreCount[songGenre[song]] += 1
+                else:
+                    genreCount[songGenre[song]] = 1
+
+                maxFreq = max(maxFreq, genreCount[songGenre[song]])
+
+            # Add user to result
+            result[user] = []
+
+            # Add to result the max count genre
+            for g in genreCount:
+                if genreCount[g] == maxFreq:
+                    result[user].append(g)
+
+        return result
