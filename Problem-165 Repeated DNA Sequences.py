755805181e97a66f6f673cc434f90b06a57fd27c
Adhish Shrivastava <adhish70@gmail.com>
2022-01-13 10:07:33 -0500

    Initial Commit

---
+++ Problem-165 Repeated DNA Sequences.py
@@ -0,0 +1,11 @@
+# 187. Repeated DNA Sequences
+# https://leetcode.com/problems/repeated-dna-sequences/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        pass
---
+++ Problem-166 Favourite Genres.py
@@ -0,0 +1,11 @@
+# Favourite Genres
+# https://github.com/adhish70/Hashing-3
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def favGenre(self, userSongs, genreSongs):
+        pass
