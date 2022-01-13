36105a6633c490a686bb820a258ffd502fa90f54
Adhish Shrivastava <adhish70@gmail.com>
2022-01-13 10:09:58 -0500

    Problem 165 done

--- Problem-165 Repeated DNA Sequences.py
+++ Problem-165 Repeated DNA Sequences.py
@@ -1,11 +1,25 @@
 # 187. Repeated DNA Sequences
 # https://leetcode.com/problems/repeated-dna-sequences/
 
-# Logic: 
+# Logic: Have sliding window of size 10. If the sequence comes 
+# for the first time, add to hashmap. If already in tha hashmap, 
+# change value and add to result.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(n - l + 1) [l = 10 here]
+# Space Complexity: O(n - l + 1)
 
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        pass
+        res = list()
+        
+        hashmap = dict()
+        
+        for i in range(len(s) - 9):
+            temp = s[i:i+10]
+            if temp not in hashmap:
+                hashmap[temp] = 0
+            else:
+                if hashmap[temp] == 0:
+                    res.append(temp)
+                    hashmap[temp] = 1
+        return res
